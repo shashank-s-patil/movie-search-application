2d97c3f8bcaa7d9f252ae2b46505d2cdb4d0ebec
Shashank Patil <146946352+shashank-s-patil@users.noreply.github.com>
2024-11-28 19:47:06 +0530

    Update tmdb.js

--- src/api/tmdb-api/tmdb.js
+++ src/api/tmdb-api/tmdb.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "YOUR_TMDB_API_KEY";
+const API_KEY = "4e6cb8a9ed65b0f6cce263394433aff4";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const fetchMovies = async (query, page = 1) => {
